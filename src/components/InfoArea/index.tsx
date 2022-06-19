d01ce7d5469bb362c93d143a2c78a5e48049c1af
isaacvrb <93296766+isaacvrb@users.noreply.github.com>
2022-06-19 09:54:30 -0300

    first commit

---
+++ src/components/InfoArea/index.tsx
@@ -0,0 +1,44 @@
+import * as C from './styles';
+import { formatCurrentMonth } from '../../helpers/dateFilter';
+import { ResumeItem } from '../ResumeItem';
+
+type Props = {
+    income: number;
+    expense: number;
+    currentMonth: string;
+    onMonthChange: (newMonth: string) => void
+}
+
+export const InfoArea = ({ income, expense, currentMonth, onMonthChange }: Props) => {
+    const handlePrevMonth = () => {
+        let [year, month] = currentMonth.split('-');
+        let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
+        currentDate.setMonth( currentDate.getMonth() - 1 );
+        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
+    }
+    const handleNextMonth = () => {
+        let [year, month] = currentMonth.split('-');
+        let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
+        currentDate.setMonth( currentDate.getMonth() + 1 );
+        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`);
+    }
+
+    return (
+        <C.Container>
+            <C.MonthArea>
+                <C.MonthArrow onClick={handlePrevMonth}>◀️</C.MonthArrow>
+                <C.MonthTitle >{formatCurrentMonth(currentMonth)}</C.MonthTitle>
+                <C.MonthArrow onClick={handleNextMonth}>▶️</C.MonthArrow>
+            </C.MonthArea>
+            <C.ResumeArea>
+                <ResumeItem title='Receitas' value={income} />
+                <ResumeItem title='Despesas' value={expense}/>
+                <ResumeItem
+                    title='Balanço'
+                    value={income - expense}
+                    color={(income - expense) < 0 ? 'red' : 'green'}
+                />
+            </C.ResumeArea>
+        </C.Container>
+    );
+}
---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2022, 5, 15), category: 'food', title: 'McDonald', value: 32.12 },
+    { date: new Date(2022, 5, 17), category: 'food', title: 'Burger King', value: 28 },
+    { date: new Date(2022, 5, 20), category: 'rent', title: 'Aluguel', value: 2300 },
+    { date: new Date(2022, 6, 30), category: 'salary', title: 'Salário ZC', value: 4500 }
+];
